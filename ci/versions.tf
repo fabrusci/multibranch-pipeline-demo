180bad968fcf30ef3a010178e22a4e6e95ca4af7
Francesco Abrusci <f.abrusci@gmail.com>
2025-03-25 08:25:55 +0100

    Fix version 'hashicorp/aws'

--- ci/versions.tf
+++ ci/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.47, < 6.0"
+      version = ">= 4.47, >= 5.62.0"
     }
     helm = {
       source  = "hashicorp/helm"
